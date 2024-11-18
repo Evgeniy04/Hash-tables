bbfe43e242b71e54b988fcfe971332009a36b205
Evgeniy04 <rontacsebe@gmail.com>
2024-11-18 12:54:36 +0700

    1.0.0

--- Lab4/Main.cpp
+++ Lab4/Main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <sstream>
-//#include "HashMap.h"
+#include "HashMap.h"
 #include "List.h"
 #include "Entry.h"
 
@@ -59,147 +59,147 @@ void PrintList(const List* list)
 	cout << endl;
 }
 
-///// <summary>
-///// Вывести все элементы словаря.
-///// </summary>
-///// <param name="hashMap">Словарь.</param>
-//void PrintHashMap(HashMap* hashMap)
-//{
-//	List** buckets = hashMap->GetBuckets();
-//	int capacity = hashMap->GetCapacity();
-//	for (int i = 0; i < capacity; i++)
-//	{
-//		if (buckets[i])
-//		{
-//			PrintList(buckets[i]);
-//			continue;
-//		}
-//		cout << "NULL" << endl;
-//	}
-//}
-//
-//void TestForHash(HashMap* hashMap)
-//{
-//	// Граничные значения для hash_size
-//	cout << hashMap->Hash("abc", 0) << endl;  // Ожидаемый результат: 0, так как hash_size = 0
-//	cout << hashMap->Hash("abc", 64) << endl; // Ожидаемый результат: корректный 64-битный хеш
-//	cout << hashMap->Hash("abc", 1) << endl;  // Ожидаемый результат: хеш из 1 бита
-//	cout << hashMap->Hash("abc", 63) << endl; // Ожидаемый результат: хеш из 63 бит
-//
-//	// Неверные значения для hash_size
-//	try {
-//		cout << hashMap->Hash("abc", -1) << endl;
-//	}
-//	catch (const invalid_argument& e) {
-//		// Ожидаемое исключение
-//		cout << e.what() << '\n';
-//	}
-//	try {
-//		cout << hashMap->Hash("abc", 65) << endl;
-//	}
-//	catch (const invalid_argument& e) {
-//		// Ожидаемое исключение
-//		cout << e.what() << '\n';
-//	}
-//
-//	// Пустая строка
-//	try {
-//		cout << hashMap->Hash("", 32) << endl;
-//	}
-//	catch (const invalid_argument& e) {
-//		// Ожидаемое исключение
-//		cout << e.what() << '\n';
-//	}
-//
-//	// Разные длины ключа
-//	cout << hashMap->Hash("a", 16) << endl;     // Короткий ключ
-//	cout << hashMap->Hash("abc", 16) << endl;   // Средний ключ
-//	cout << hashMap->Hash("abcdefghijklmnopqrstuvwxyz", 16) << endl; // Длинный ключ
-//
-//	// Различные символы
-//	cout << hashMap->Hash("12345", 32) << endl;             // Цифры
-//	cout << hashMap->Hash("ABCDE", 32) << endl;             // Верхний регистр
-//	cout << hashMap->Hash("abcde", 32) << endl;             // Нижний регистр
-//	cout << hashMap->Hash("a b c d e", 32) << endl;         // С пробелами
-//	cout << hashMap->Hash("!@#$%^&*()", 32) << endl;        // Нестандартные символы
-//
-//	// Сочетания границ
-//	try {
-//		cout << hashMap->Hash("", 0) << endl;               // Пустой ключ, минимальный hash_size
-//	}
-//	catch (const invalid_argument& e) {
-//		// Ожидаемое исключение
-//		cout << e.what() << '\n';
-//	}
-//	cout << hashMap->Hash("abcdefghijklmnopqrstuvwxyz", 64) << endl; // Максимальный ключ, максимальный hash_size
-//}
-//
-//void ASSERT(bool value)
-//{
-//	if (!value)
-//	{
-//		throw new exception("TestForHashMap.");
-//	}
-//}
-//
-//void TestForHashMap(HashMap& hashMap)
-//{
-//	// Добавление уникальных записей
-//	hashMap.Insert(new Entry("key1", "value1"));
-//	hashMap.Insert(new Entry("key2", "value2"));
-//
-//	ASSERT(hashMap.Find("key1") == "value1");
-//	ASSERT(hashMap.Find("key2") == "value2");
-//
-//	// Обновление записи с одинаковым ключом
-//	hashMap.Insert(new Entry("key", "value1"));
-//	hashMap.Insert(new Entry("key", "value2"));
-//
-//	ASSERT(hashMap.Find("key") == "value2"); // Значение должно обновиться
-//
-//	// Длинные ключ и значение
-//	std::string longKey(1000, 'a');
-//	std::string longValue(1000, 'b');
-//	hashMap.Insert(new Entry(longKey, longValue));
-//	ASSERT(hashMap.Find(longKey) == longValue);
-//
-//	// Большое число записей
-//	for (int i = 0; i < 10000; ++i) {
-//		hashMap.Insert(new Entry("key" + std::to_string(i), "value" + std::to_string(i)));
-//	}
-//	for (int i = 0; i < 10000; ++i) {
-//		ASSERT(hashMap.Find("key" + std::to_string(i)) == "value" + std::to_string(i));
-//	}
-//
-//
-//	// Обработка коллизий
-//	hashMap.Insert(new Entry("key1", "value1"));
-//	hashMap.Insert(new Entry("key2", "value2"));
-//
-//	ASSERT(hashMap.Find("key1") == "value1");
-//	ASSERT(hashMap.Find("key2") == "value2");
-//
-//
-//	// Исключение при отсутствии ключа
-//	try {
-//		hashMap.Find("nonexistent_key");
-//	}
-//	catch (const std::invalid_argument& e) {
-//		ASSERT(true); // Исключение ожидается
-//	}
-//	PrintHashMap(&hashMap);
-//}
+/// <summary>
+/// Вывести все элементы словаря.
+/// </summary>
+/// <param name="hashMap">Словарь.</param>
+void PrintHashMap(HashMap* hashMap)
+{
+	List** buckets = hashMap->GetBuckets();
+	int capacity = hashMap->GetCapacity();
+	for (int i = 0; i < capacity; i++)
+	{
+		if (buckets[i])
+		{
+			PrintList(buckets[i]);
+			continue;
+		}
+		cout << "NULL" << endl;
+	}
+}
+
+void TestForHash(HashMap* hashMap)
+{
+	// Граничные значения для hash_size
+	cout << hashMap->Hash("abc", 0) << endl;  // Ожидаемый результат: 0, так как hash_size = 0
+	cout << hashMap->Hash("abc", 64) << endl; // Ожидаемый результат: корректный 64-битный хеш
+	cout << hashMap->Hash("abc", 1) << endl;  // Ожидаемый результат: хеш из 1 бита
+	cout << hashMap->Hash("abc", 63) << endl; // Ожидаемый результат: хеш из 63 бит
+
+	// Неверные значения для hash_size
+	try {
+		cout << hashMap->Hash("abc", -1) << endl;
+	}
+	catch (const invalid_argument& e) {
+		// Ожидаемое исключение
+		cout << e.what() << '\n';
+	}
+	try {
+		cout << hashMap->Hash("abc", 65) << endl;
+	}
+	catch (const invalid_argument& e) {
+		// Ожидаемое исключение
+		cout << e.what() << '\n';
+	}
+
+	// Пустая строка
+	try {
+		cout << hashMap->Hash("", 32) << endl;
+	}
+	catch (const invalid_argument& e) {
+		// Ожидаемое исключение
+		cout << e.what() << '\n';
+	}
+
+	// Разные длины ключа
+	cout << hashMap->Hash("a", 16) << endl;     // Короткий ключ
+	cout << hashMap->Hash("abc", 16) << endl;   // Средний ключ
+	cout << hashMap->Hash("abcdefghijklmnopqrstuvwxyz", 16) << endl; // Длинный ключ
+
+	// Различные символы
+	cout << hashMap->Hash("12345", 32) << endl;             // Цифры
+	cout << hashMap->Hash("ABCDE", 32) << endl;             // Верхний регистр
+	cout << hashMap->Hash("abcde", 32) << endl;             // Нижний регистр
+	cout << hashMap->Hash("a b c d e", 32) << endl;         // С пробелами
+	cout << hashMap->Hash("!@#$%^&*()", 32) << endl;        // Нестандартные символы
+
+	// Сочетания границ
+	try {
+		cout << hashMap->Hash("", 0) << endl;               // Пустой ключ, минимальный hash_size
+	}
+	catch (const invalid_argument& e) {
+		// Ожидаемое исключение
+		cout << e.what() << '\n';
+	}
+	cout << hashMap->Hash("abcdefghijklmnopqrstuvwxyz", 64) << endl; // Максимальный ключ, максимальный hash_size
+}
+
+void ASSERT(bool value)
+{
+	if (!value)
+	{
+		throw new exception("TestForHashMap.");
+	}
+}
+
+void TestForHashMap(HashMap& hashMap)
+{
+	// Добавление уникальных записей
+	hashMap.Insert(new Entry("key1", "value1"));
+	hashMap.Insert(new Entry("key2", "value2"));
+
+	ASSERT(hashMap.Find("key1") == "value1");
+	ASSERT(hashMap.Find("key2") == "value2");
+
+	// Обновление записи с одинаковым ключом
+	hashMap.Insert(new Entry("key", "value1"));
+	hashMap.Insert(new Entry("key", "value2"));
+
+	ASSERT(hashMap.Find("key") == "value2"); // Значение должно обновиться
+
+	// Длинные ключ и значение
+	std::string longKey(1000, 'a');
+	std::string longValue(1000, 'b');
+	hashMap.Insert(new Entry(longKey, longValue));
+	ASSERT(hashMap.Find(longKey) == longValue);
+
+	// Большое число записей
+	for (int i = 0; i < 10000; ++i) {
+		hashMap.Insert(new Entry("key" + std::to_string(i), "value" + std::to_string(i)));
+	}
+	for (int i = 0; i < 10000; ++i) {
+		ASSERT(hashMap.Find("key" + std::to_string(i)) == "value" + std::to_string(i));
+	}
+
+
+	// Обработка коллизий
+	hashMap.Insert(new Entry("key1", "value1"));
+	hashMap.Insert(new Entry("key2", "value2"));
+
+	ASSERT(hashMap.Find("key1") == "value1");
+	ASSERT(hashMap.Find("key2") == "value2");
+
+
+	// Исключение при отсутствии ключа
+	try {
+		hashMap.Find("nonexistent_key");
+	}
+	catch (const std::invalid_argument& e) {
+		ASSERT(true); // Исключение ожидается
+	}
+	PrintHashMap(&hashMap);
+}
 
 int main()
 {
-	//HashMap* hashMap = new HashMap();
+	HashMap* hashMap = new HashMap();
 	List* list = new List();
 
 	bool flag = true;
 	while (flag)
 	{
 		cout << "Current HashMap: " << endl;
-		//PrintHashMap(hashMap);
+		PrintHashMap(hashMap);
 		cout << "Actions:" << endl;
 		cout << "[1] Insert" << endl;
 		cout << "[2] Remove" << endl;
@@ -210,7 +210,7 @@ int main()
 		system("cls");
 		switch (cmd)
 		{
-			/*case 1:
+			case 1:
 			{
 				cout << "[1] Enter the key: ";
 				string key = CinString();
@@ -240,7 +240,7 @@ int main()
 				}
 				break;
 			}
-			case 4:
+			/*case 4:
 			{
 				for (int i = 0; i < 1000000; ++i) {
 					hashMap->Insert(new Entry("key" + std::to_string(i), "value" + std::to_string(i)));
@@ -253,7 +253,7 @@ int main()
 					hashMap->Remove("key" + std::to_string(i));
 				}
 				break;
-			}*/
+			}
 			case 6:
 			{
 				for (int i = 0; i < 500000; ++i) {
@@ -299,7 +299,7 @@ int main()
 
 				cout << result;
 				break;
-			}
+			}*/
 			case 0:
 			{
 				flag = false;
